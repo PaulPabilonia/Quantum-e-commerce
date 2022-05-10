8901137f335f765e7b4cb790c11e24a93982325a
PaulPabilonia <paulpabilonia123@gmail.com>
2022-05-10 22:29:17 +0800

    Changes for quanta to Quantum

--- Quantum/asgi.py
+++ Quantum/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quantum project.
+ASGI config for Quantum project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quantum.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quantum.settings')
 
 application = get_asgi_application()
