1753a1c25f19dca69efd19693e4b09662794b87a
PaulPabilonia <paulpabilonia123@gmail.com>
2022-05-10 21:51:21 +0800

    changes for Quantum to quantum

--- Quantum/urls.py
+++ Quantum/urls.py
@@ -1,4 +1,4 @@
-"""Quantum URL Configuration
+"""quantum URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
